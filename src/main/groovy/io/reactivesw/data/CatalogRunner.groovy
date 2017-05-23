9baeada59e8459b46b3fcc2007afb35ed907416b
Davis <davis.dai@go6d.com>
2017-05-23 10:42:06 +0800

    fix url bug

--- src/main/groovy/io/reactivesw/data/CatalogRunner.groovy
+++ src/main/groovy/io/reactivesw/data/CatalogRunner.groovy
@@ -39,26 +39,26 @@ public def static addData() {
 
     println("-------------------- create product  -----------------------")
     def blouse = ProductDataFactory.blouse
-    blouse['categories'][0]['id'] = "ed79f652-ed9f-49ca-9e34-bc9578768762"
-    blouse['productType']['id'] = "3c743a07-1a39-4925-843f-bb4d3127779c"
+    blouse['categories'][0]['id'] = clothesCategory.id
+    blouse['productType']['id'] = clotheProductType.id
     def blouseProduct = RestClientFactory.post(UrlUtils.PRODUCTURL, blouse)
     println("create blouse product, id is: " + blouseProduct.id)
 
     def levisJean = ProductDataFactory.levisJean
-    levisJean['categories'][0]['id'] = "581f0d41-98b8-4e61-be95-8098eef89b71"
-    levisJean['productType']['id'] = "784b277b-686b-43c6-b5a7-d15317748ab2"
+    levisJean['categories'][0]['id'] = pantsCategory.id
+    levisJean['productType']['id'] = pantsProductType.id
     def levisJeanProduct = RestClientFactory.post(UrlUtils.PRODUCTURL, levisJean)
     println("create levisJean product, id is: " + levisJeanProduct.id)
 
     def alienware15R2 = ProductDataFactory.alienware15R2
-    alienware15R2['categories'][0]['id'] = "52cb49ef-6fd3-477f-8978-f4e0d2e9aefa"
-    alienware15R2['productType']['id'] = "946b3058-6f97-43d7-af49-1a885cf5331f"
+    alienware15R2['categories'][0]['id'] = lapTopCategory.id
+    alienware15R2['productType']['id'] = lapTopProductType.id
     def alienware15R2Product = RestClientFactory.post(UrlUtils.PRODUCTURL, alienware15R2)
     println("create alienware15R2 product, id is: " + alienware15R2Product.id)
 
     def onePlus3T = ProductDataFactory.onePlus3T
-    onePlus3T['categories'][0]['id'] = "9df106d4-e063-48fb-8061-21e0e4c6db56"
-    onePlus3T['productType']['id'] = "ef4233ae-05f9-4255-9985-09badd747ceb"
+    onePlus3T['categories'][0]['id'] = smartPhoneCategory.id
+    onePlus3T['productType']['id'] = smartPhoneProductType.id
     def onePlus3TProduct = RestClientFactory.post(UrlUtils.PRODUCTURL, onePlus3T)
     println("create iphone10 product, id is: " + onePlus3TProduct.id)
 
--- src/main/groovy/io/reactivesw/data/util/UrlUtils.groovy
+++ src/main/groovy/io/reactivesw/data/util/UrlUtils.groovy
@@ -13,19 +13,23 @@ class UrlUtils {
      * category url.
      */
     public static String CATEGORYURL = ROOTURL + "categories/"
+//    public static String CATEGORYURL = "http://localhost:8082/"
 
     /**
      * product type url.
      */
     public static String PRODUCTTYPEURL = ROOTURL + "product-types/"
+//    public static String PRODUCTTYPEURL = "http://localhost:8089/"
 
     /**
      * product url.
      */
     public static String PRODUCTURL = ROOTURL + "products/"
+//    public static String PRODUCTURL = "http://localhost:8088/"
 
     /**
      * inventory url.
      */
     public static String INVENTORYURL = ROOTURL + "inventory/"
+//    public static String INVENTORYURL = "http://localhost:8085/"
 }
